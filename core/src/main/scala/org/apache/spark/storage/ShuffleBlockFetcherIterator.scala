b0a3fc23eeaecd44248084a1d75c975806e4b29f
jerryshao <saisai.shao@intel.com>
2014-11-05 09:10:14 +0800

    Changes to rebase to the latest master branch

--- core/src/main/scala/org/apache/spark/storage/ShuffleBlockFetcherIterator.scala
+++ core/src/main/scala/org/apache/spark/storage/ShuffleBlockFetcherIterator.scala
@@ -32,7 +32,7 @@ import org.apache.spark.util.{CompletionIterator, Utils}
 private[spark]
 final class ShuffleBlockFetcherIterator(
     context: TaskContext,
-    blockTransferService: BlockTransferService,
+    shuffleClient: ShuffleClient,
     blockManager: BlockManager,
     blocksByAddress: Seq[(BlockManagerId, Seq[(BlockId, Long)])],
     serializer: Serializer,
@@ -41,7 +41,7 @@ final class ShuffleBlockFetcherIterator(
 
   val shuffleRawBlockFetcherItr = new ShuffleRawBlockFetcherIterator(
     context,
-    blockTransferService,
+    shuffleClient,
     blockManager,
     blocksByAddress,
     maxBytesInFlight)
